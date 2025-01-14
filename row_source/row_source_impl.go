d043f6856dc7249c152c80e32030caec761610eb
kai <kai@turbot.com>
2025-01-14 16:56:06 +0000

    sort from time init

--- row_source/row_source_impl.go
+++ row_source/row_source_impl.go
@@ -3,13 +3,13 @@ package row_source
 import (
 	"context"
 	"fmt"
+	"github.com/turbot/tailpipe-plugin-sdk/constants"
 	"log/slog"
 	"time"
 
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/pipe-fittings/utils"
 	"github.com/turbot/tailpipe-plugin-sdk/collection_state"
-	"github.com/turbot/tailpipe-plugin-sdk/constants"
 	"github.com/turbot/tailpipe-plugin-sdk/context_values"
 	"github.com/turbot/tailpipe-plugin-sdk/events"
 	"github.com/turbot/tailpipe-plugin-sdk/observable"
@@ -50,30 +50,14 @@ func (r *RowSourceImpl[S, T]) RegisterSource(source RowSource) {
 // it is responsible for parsing the source config and configuring the source
 func (r *RowSourceImpl[S, T]) Init(_ context.Context, params *RowSourceParams, opts ...RowSourceOption) error {
 	slog.Info(fmt.Sprintf("Initializing RowSourceImpl %p, impl %p", r, r.Source))
-
+	if r.NewCollectionStateFunc == nil {
+		return fmt.Errorf("NewCollectionStateFunc not set")
+	}
 	// apply options to the Source (as options will be dependent on the outer type)
 	for _, opt := range opts {
 		if err := opt(r.Source); err != nil {
 			return err
 		}
-	}
-
-	// set the from time
-	r.FromTime = params.From
-	// // if no from time was passed, set it to the end time of the collection state
-	if r.FromTime.IsZero() {
-		r.FromTime = r.CollectionState.GetEndTime()
-	} else {
-		// TODO KAI update collection state based on from time
-		// -- if from time is before collection state start time, clear colleciton state
-		// -- if from time is during collection state, update end time to the from tiume
-		// -- if from time is after collection state end time, clear collection state (???
-	}
-
-	// if from is not set (either by explicitly passing is as an arg, or from the collection state end time) set it now
-	// to the default (7 days
-	if r.FromTime.IsZero() {
-		r.FromTime = time.Now().Add(-constants.DefaultInitialCollectionPeriod)
 	}
 
 	err := r.initialiseConfig(params.SourceConfigData)
@@ -86,16 +70,33 @@ func (r *RowSourceImpl[S, T]) Init(_ context.Context, params *RowSourceParams, o
 		return err
 	}
 
-	// if no collection state has been set already (by calling SetCollectionState) create empty collection state
-	// TODO #design is it acceptable to have no collection state? we should put nil checks round access to it
-	if r.CollectionState == nil && r.NewCollectionStateFunc != nil {
-		slog.Info("Creating empty collection state")
-		r.CollectionState = r.NewCollectionStateFunc()
-		// initialise the collection state
-		err = r.CollectionState.Init(r.Config, params.CollectionStatePath)
-		if err != nil {
-			return err
+	// create empty collection state
+	slog.Info("Creating empty collection state")
+	r.CollectionState = r.NewCollectionStateFunc()
+	// initialise the collection state - this ill load itself form json (if JSON file exists)
+	err = r.CollectionState.Init(r.Config, params.CollectionStatePath)
+	if err != nil {
+		return err
+	}
+
+	// set the from time
+	r.FromTime = params.From
+	// // if no from time was passed, set it to the end time of the collection state
+	if r.FromTime.IsZero() {
+		if r.CollectionState != nil {
+			r.FromTime = r.CollectionState.GetEndTime()
 		}
+	} else {
+		// TODO KAI update collection state based on from time
+		// -- if from time is before collection state start time, clear colleciton state
+		// -- if from time is during collection state, update end time to the from tiume
+		// -- if from time is after collection state end time, clear collection state (???
+	}
+
+	// if from is not set (either by explicitly passing is as an arg, or from the collection state end time) set it now
+	// to the default (7 days
+	if r.FromTime.IsZero() {
+		r.FromTime = time.Now().Add(-constants.DefaultInitialCollectionPeriod)
 	}
 
 	return nil
