a0e1a28c3de0188c2e5ce5592fb31fff4da31798
kai <kai@turbot.com>
2025-04-24 11:23:21 +0100

    linting

--- plugin/serve.go
+++ plugin/serve.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/turbot/go-kit/helpers"
 	"google.golang.org/grpc"
-	_ "net/http/pprof"
+	//_ "net/http/pprof"
 )
 
 // ServeOpts are the configurations to serve a plugin.
@@ -56,7 +56,7 @@ func newGRPCServer(options []grpc.ServerOption) *grpc.Server {
 }
 
 func setupPprof() {
-	slog.Info("PLUGIN IS PROFILING!!!!")
+	slog.Info("PROFILING!!!!")
 	go func() {
 		listener, err := net.Listen("tcp", "localhost:0")
 		if err != nil {
